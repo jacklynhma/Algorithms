a61274d7c8cbc3108464e62b3e166741d01432b6
jacklynhma <jacklynhma@gmail.com>
2018-04-08 17:59:35 -0400

    binary search

---
+++ binary_search.rb
@@ -0,0 +1,35 @@
+def binary_search()
+  x = 50
+  array = [2, 4, 6, 8, 12, 19, 20, 30, 50, 93, 100, 200]
+  max = array.length - 1
+  min = 0
+
+  answer = ""
+  while answer == ""
+    middle_index = ( min + ((max - min )/ 2).to_i )
+
+    if x == array[middle_index]
+      answer =  middle_index
+    elsif array[min..max].length == 2
+      if array[max] != x && array[min] != x
+        answer = "x does not exist in the array"
+      else
+        #the min is already checked in line 9
+        middle_index = max
+        answer = middle_index
+      end
+    elsif array[middle_index] < x
+      min = middle_index
+    elsif array[middle_index] > x
+      max = middle_index
+    end
+  end
+  if answer == 8
+    return "Correct"
+  else
+    return "Result: #{answer}"
+  end
+end
+
+# Runtime: 0.03s
+puts binary_search
